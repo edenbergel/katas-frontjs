4c5f91ccc2b54305e2de14a8d265601dac5cc535
Eden BERGEL <eb@wshop-cloudcommerce.com>
2022-05-12 13:07:25 +0200

    update ui header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,36 +11,38 @@ function Header(to) {
   return (
     <>
       <nav className="navbar border-bottom border-dark">
-        <div className="container-fluid justify-content-center">
-          <div className="d-block">
-            <h1 className="navbar-brand fw-bold fs-2">BEERS</h1>
-            
-            <ul className="nav justify-content-center">
-              <li className={`nav-item ${match.pathname === '/' ? 'active' : ''}`}>
-                <Link to='/' className={`nav-link fs-6 me-5 ${match.pathname === '/' ? 'active' : ''}`}>
-                  SHOP
-                </Link>
-              </li>
+        <div className="container-fluid justify-content-between ms-4 me-5">
+          <h1 className="navbar-brand fw-bold fs-2">
+            <Link to='/' className="text-dark">
+              BEERS
+            </Link>
+          </h1>
+          
+          <ul className="nav justify-content-center">
+            <li className={`nav-item ${match.pathname === '/' ? 'active' : ''}`}>
+              <Link to='/' className={`nav-link text-dark fs-6 me-5 ${match.pathname === '/' ? 'active' : ''}`}>
+                SHOP
+              </Link>
+            </li>
 
-              <li className={`nav-item ${match.pathname === '/cart/' ? 'is-active' : ''}`}>
-                <Link to='/cart' className={`nav-link fs-6 ms-5 position-relative ${window.location.href.indexOf('cart/') > -1 ? 'active' : ''}`}>
-                  CART 
-                  
-                  { state.cart.length !== 0 || cartStorage !== null ?
-                      cartStorage.length === 0 ?
-                        <span className="position-absolute top-2 start-100 translate-middle px-2 bg-dark rounded-circle">
-                          <span className="text-white">{state.cart.length}</span>
-                        </span>
-                        :
-                        <span className="position-absolute top-2 start-100 translate-middle px-2 bg-dark rounded-circle">
-                            <span className="text-white">{cartStorage.length}</span> 
-                        </span>
-                    : ""
-                  }
-                </Link>
-              </li>
-            </ul>
-          </div>
+            <li className={`nav-item ${match.pathname === '/cart/' ? 'is-active' : ''}`}>
+              <Link to='/cart' className={`nav-link text-dark fs-6 ms-5 position-relative ${window.location.href.indexOf('cart/') > -1 ? 'active' : ''}`}>
+                CART 
+                
+                { state.cart.length !== 0 || cartStorage !== null ?
+                    cartStorage.length === 0 ?
+                      <span className="position-absolute top-2 start-100 translate-middle px-2 bg-dark rounded-circle">
+                        <span className="text-white">{state.cart.length}</span>
+                      </span>
+                      :
+                      <span className="position-absolute top-2 start-100 translate-middle px-2 bg-dark rounded-circle">
+                          <span className="text-white">{cartStorage.length}</span> 
+                      </span>
+                  : ""
+                }
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
